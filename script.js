4bc9f41f4667082dd30f65d6d257f3bee86ef0f3
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-03-08 20:45:17 +0530

    Updated js file

--- script.js
+++ script.js
@@ -6,20 +6,27 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
+const enemyImage = new Image();
+enemyImage.src = 'enemy1.png';
+
 
 class Enemy{
     constructor(){
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.width = 200;
-        this.height = 200;
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
     }
     update(){
-        this.x++;
-        this.y++;
+        this.x += this.speed;
+        this.y += this.speed;
     }
     draw(){
-        ctx.fillRect(this.x,this.y,this.width, this.height);
+        // ctx.fillRect(this.x,this.y,this.width, this.height);
+        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y,this.width,this.height);
     }
 };
 for(let i = 0; i < numberOfEnemies; i++){
@@ -28,8 +35,10 @@ for(let i = 0; i < numberOfEnemies; i++){
 console.log(enemiesArray);
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // enemy1.update();
-    // enemy1.draw();
+    enemiesArray.forEach(enemy =>{
+        enemy.update();
+        enemy.draw();
+    })
     requestAnimationFrame(animate);
 }
 
