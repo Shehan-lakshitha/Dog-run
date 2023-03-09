d8f0a3c1f9ad34c7be519d0b4f77b8e665b97059
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-03-09 20:18:30 +0530

    upadated js file

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'enemy1.png';
+let gameFrame = 0;
 
 
 class Enemy{
     constructor(){
+        this.image = new Image();
+        this.image.src = 'enemy1.png';
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.speed = Math.random() * 4 - 2;
@@ -19,14 +20,20 @@ class Enemy{
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.frame = 2;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update(){
         this.x += this.speed;
         this.y += this.speed;
+        // animate sprites
+        if (gameFrame % this.flapSpeed == 0){
+            this.frame > 4 ? this.frame = 0 : this.frame ++;
+        }
+        
     }
     draw(){
-        // ctx.fillRect(this.x,this.y,this.width, this.height);
-        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y,this.width,this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y,this.width,this.height);
     }
 };
 for(let i = 0; i < numberOfEnemies; i++){
@@ -38,7 +45,8 @@ function animate(){
     enemiesArray.forEach(enemy =>{
         enemy.update();
         enemy.draw();
-    })
+    });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 
