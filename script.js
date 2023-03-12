cfa2411afb49d0233c493eacfefbb12ccc2377a7
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-03-12 22:15:10 +0530

    updated js file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 300;
+const numberOfEnemies = 30;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -13,19 +13,19 @@ class Enemy{
     constructor(){
         this.image = new Image();
         this.image.src = 'enemy1.png';
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
         // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 2;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update(){
-        this.x += Math.random() * 3 - 2.5;
-        this.y +=  Math.random() * 3 - 2.5;
+        this.x += Math.random() * 5 - 2.5;
+        this.y +=  Math.random() * 5 - 2.5;
         // animate sprites
         if (gameFrame % this.flapSpeed == 0){
             this.frame > 4 ? this.frame = 0 : this.frame ++;
