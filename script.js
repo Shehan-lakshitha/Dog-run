17e96097aeeb78ab5290d243ab69497c3f0b7f92
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-03-18 20:48:38 +0530

    Updated the movement pattern

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ class Enemy{
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
-        this.angleSpeed = Math.random() * 2;
-        this.curve = Math.random()  * 200;
+        this.angleSpeed = Math.random() * 1.5 + 0.5;
+        this.curve = Math.random()  * 200 + 50;
     }
     update(){
-        this.x = this.curve * Math.sin(this.angle * Math.PI/180) + (canvas.width/2 - this.width/2);
-        this.y = this.curve * Math.tan(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2);
+        this.x = this.curve * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        this.y = this.curve * Math.cos(this.angle * Math.PI/360) + (canvas.height/2 - this.height/2);
         this.angle += this.angleSpeed;
         if(this.x + this.width < 0) this.x = canvas.width; //creates a loop in enemies
         // animate sprites
