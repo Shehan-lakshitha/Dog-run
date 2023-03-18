606f8ffb4d55513146908d40b7e1bb78d334f13d
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-03-18 21:47:20 +0530

    Circular enemy movement

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 20;
+const numberOfEnemies = 200;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -23,11 +23,11 @@ class Enemy{
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
         this.angleSpeed = Math.random() * 1.5 + 0.5;
-        this.curve = Math.random()  * 200 + 50;
+        //this.curve = Math.random()  * 200 + 50;
     }
     update(){
-        this.x = this.curve * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
-        this.y = this.curve * Math.cos(this.angle * Math.PI/360) + (canvas.height/2 - this.height/2);
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        this.y = canvas.width * Math.cos(this.angle * Math.PI/100) + (canvas.height/2 - this.height/2);
         this.angle += this.angleSpeed;
         if(this.x + this.width < 0) this.x = canvas.width; //creates a loop in enemies
         // animate sprites
