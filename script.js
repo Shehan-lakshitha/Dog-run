5fc2f465c1eeaa0e964c4424c21993820cc2a571
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-03-19 20:30:17 +0530

    changed the no. of enemies

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 200;
+const numberOfEnemies = 150;
 const enemiesArray = [];
 
 let gameFrame = 0;
